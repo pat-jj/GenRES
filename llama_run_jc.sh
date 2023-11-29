231d8000b253e2666e4bffea8e78eb821c28dfde
linjc16 <250551019@qq.com>
2023-11-28 22:25:33 -0600

    add vicuna

---
+++ llama_run_jc.sh
@@ -0,0 +1,39 @@
+# export CUDA_VISIBLE_DEVICES=0,5,6
+DATASET=$1
+MODEL_NAME=$2
+GPU=$3
+PROMPT_DEMO=$4
+
+CUDA_VISIBLE_DEVICES=$3 python3 gre_run.py \
+    --model_family llama \
+    --model_name $MODEL_NAME \
+    --model_cache_dir /data/pj20/.cache \
+    --dataset $DATASET \
+    --prompt $PROMPT_DEMO \
+    --exp_id 1 \
+
+# python3 gre_run.py \
+#     --model_family llama \
+#     --model_name llama-2-70b \
+#     --model_cache_dir /data/pj20/.cache \
+#     --dataset wiki20m_rand_500 \
+#     --prompt general_bag \
+#     --exp_id 1 \
+
+
+# CUDA_VISIBLE_DEVICES=1 python3 gre_run.py \
+#     --model_family llama \
+#     --model_name llama-2-7b \
+#     --model_cache_dir /data/pj20/.cache \
+#     --dataset wiki20m_rand_500 \
+#     --prompt general_bag \
+#     --exp_id 1 \
+
+
+# CUDA_VISIBLE_DEVICES=2 python3 gre_run.py \
+#     --model_family llama \
+#     --model_name llama-2-7b \
+#     --model_cache_dir /data/pj20/.cache \
+#     --dataset tacred_rand_800 \
+#     --prompt general_sent \
+#     --exp_id 1 \
---
+++ vicuna_run.sh
@@ -0,0 +1,1 @@
+bash llama_run_jc.sh wiki20m_rand_500 vicuna-1.5-7b 1 general_bag
