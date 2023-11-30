1bc32f3e4ee60cadd293ab8ffe24b67ce3b42593
Patrick <pj20@illinois.edu>
2023-11-30 11:43:11 -0600

    update

--- run_models/galactica.py
+++ run_models/galactica.py
@@ -27,9 +27,9 @@ def galactica_model_inference(tokenizer, model, text, prompt):
     num_tokens = len(tokens)  # Number of tokens in the input text
 
     # Set max_new_tokens to twice the number of tokens in the text
-    max_new_tokens = 3 * num_tokens
+    max_new_tokens = 8 * num_tokens
 
     generated_ids = model.generate(input_ids, max_new_tokens=max_new_tokens, do_sample=False)
-    decoded = tokenizer.batch_decode(generated_ids[0])
+    decoded = tokenizer.batch_decode(generated_ids)
     
-    return decoded
+    return decoded[0]
--- run_models/llama.py
+++ run_models/llama.py
@@ -81,7 +81,7 @@ def llama_model_inference(tokenizer, model, text, prompt, device='cuda'):
     num_tokens = len(tokens)  # Number of tokens in the input text
 
     # Set max_new_tokens to twice the number of tokens in the text
-    max_new_tokens = 2 * num_tokens
+    max_new_tokens = 8 * num_tokens
 
     generated_ids = model.generate(model_inputs, max_new_tokens=max_new_tokens, do_sample=False)
     decoded = tokenizer.batch_decode(generated_ids)
