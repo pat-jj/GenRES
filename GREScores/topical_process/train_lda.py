9495bbc7a27ecb35549f443483866e7af370db22
Patrick <pj20@illinois.edu>
2023-11-30 19:37:56 -0600

    add trained LDA models

--- GREScores/topical_process/train_lda.py
+++ GREScores/topical_process/train_lda.py
@@ -1,9 +1,10 @@
-import gensim
 from gensim import corpora
 from gensim.models.ldamodel import LdaModel
 from gensim.matutils import kullback_leibler
 import nltk
 import pickle
+import json
+from tqdm import tqdm
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 nltk.download('stopwords')
@@ -14,21 +15,25 @@ def preprocess(text):
     words = word_tokenize(text)
     return [word for word in words if word not in stop_words and word.isalnum()]
 
-# Assuming all_source_texts is a list of your source texts
-all_source_texts = ["Text 1", "Text 2", ...]  # Your source texts here
+topic_nums = {'cdr': 50, 'docred': 100, 'nyt10m': 150, 'wiki20m': 150, 'tacred': 150, 'wiki80': 150}
+datasets = ['cdr', 'docred', 'nyt10m', 'wiki20m', 'tacred', 'wiki80']
 
-# Preprocess all texts
-processed_texts = [preprocess(text) for text in all_source_texts]
+for dataset in tqdm(datasets):
+    print(f"Training LDA model for {dataset}...")
+    all_source_texts = json.load(open(f'./{dataset}_all_text.json', 'r'))
 
-# Create dictionary and corpus for LDA
-dictionary = corpora.Dictionary(processed_texts)
-corpus = [dictionary.doc2bow(text) for text in processed_texts]
+    # Preprocess all texts
+    processed_texts = [preprocess(text) for text in all_source_texts]
 
-with open('dictionary.pkl', 'wb') as f:
-    pickle.dump(dictionary, f)
+    # Create dictionary and corpus for LDA
+    dictionary = corpora.Dictionary(processed_texts)
+    corpus = [dictionary.doc2bow(text) for text in processed_texts]
 
-# Train LDA model
-lda = LdaModel(corpus, num_topics=10, id2word=dictionary)
+    with open(f'./{dataset}_dictionary.pkl', 'wb') as f:
+        pickle.dump(dictionary, f)
 
-with open('lda.pkl', 'wb') as f:
-    pickle.dump(lda, f)
+    # Train LDA model
+    lda = LdaModel(corpus, num_topics=topic_nums[dataset], id2word=dictionary)
+
+    with open(f'./{dataset}_lda.pkl', 'wb') as f:
+        pickle.dump(lda, f)
